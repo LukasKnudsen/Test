ec7dadef2f9b0c0d53748fe70697766c7514602c
Lukas Eggers Knudsen <yaboi@MBPtilhndeLukas.home>
2024-08-24 21:03:20 +0200

    test

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test{
+public static void main(String[] args) {
+    System.out.println("hello");
+    System.out.println("virker det?");
+    int number = 10;
+    
+    int[] array = new int [3];
+    System.out.println(array.length);
+   
+}
+}
